15409d1c6a92f82292d7ee04784f453f18b97116
Nicolás Kruk <nicolasgkruk@gmail.com>
2020-04-05 18:49:30 +0200

    Fix for issues arising from researching excerpts.

--- app/assets/javascripts/excerpts-choice-for-new-sign.js
+++ app/assets/javascripts/excerpts-choice-for-new-sign.js
@@ -14,27 +14,42 @@ $(document).on("turbolinks:load", function() {
         return 'signs/new?' + excerptIdsToParams;
     };
 
-    $('.add-excerpt-to-sign').click(function() {
-        const excerptId = $(this).attr('id').slice(12);
-        $(this).addClass('no-show');
-        $("#remove-excerpt-" + excerptId).removeClass('no-show');
-        $(this).closest("li").addClass('selected-excerpt');
-        const addLiHtml = $(this).closest("li").html();
-        chosenExcerpts.push({id: excerptId, html: addLiHtml});
+    function addListeners() {
+        $('.add-excerpt-to-sign').click(function() {
+            const excerptId = $(this).attr('id').slice(12);
+            $(this).addClass('no-show');
+            $(".remove-excerpt-" + excerptId).removeClass('no-show');
+            $(this).closest("li").addClass('selected-excerpt');
+            const addLiHtml = $(this).closest("li").html();
+            chosenExcerpts.push({id: excerptId, html: addLiHtml});
 
-        // Update href of .create-sign-with-chosen-excerpts
-        $('.create-sign-with-chosen-excerpts').attr('href', updateHref());
-    });
+            // Update href of .create-sign-with-chosen-excerpts
+            $('.create-sign-with-chosen-excerpts').attr('href', updateHref());
+        });
 
-    $('.remove-excerpt-from-sign').click(function() {
-        const excerptId = $(this).attr('id').slice(15);
-        $(this).addClass('no-show');
-        $("#add-excerpt-" + excerptId).removeClass('no-show');
-        $(this).closest("li").removeClass('selected-excerpt');
-        chosenExcerpts = chosenExcerpts.filter(x => x.id !== excerptId);
+        $('.remove-excerpt-from-sign').click(function() {
+            const excerptId = $(this).attr('id').slice(15);
+            $(this).addClass('no-show');
+            $(".add-excerpt-" + excerptId).removeClass('no-show');
+            $(this).closest("li").removeClass('selected-excerpt');
+            chosenExcerpts = chosenExcerpts.filter(x => x.id !== excerptId);
 
-        // Update href of .create-sign-with-chosen-excerpts
-        $('.create-sign-with-chosen-excerpts').attr('href', updateHref());
+            // Update href of .create-sign-with-chosen-excerpts
+            $('.create-sign-with-chosen-excerpts').attr('href', updateHref());
+        });
+    };
+
+    addListeners();
+
+    $('#search-results').on('DOMSubtreeModified', function(){
+        addListeners();
+
+        //check the ids of those already selected and do the same we do when we click add-excerpt-to-sign
+        for (let id of chosenExcerpts.map(x => x.id)) {
+            $('.add-excerpt-' + id).addClass('no-show');
+            $(".remove-excerpt-" + id).removeClass('no-show');
+            $('.add-excerpt-' + id).closest("li").addClass('selected-excerpt');
+        }
     });
 
     $('#show-selected-excerpts').click(() => {
@@ -53,9 +68,9 @@ $(document).on("turbolinks:load", function() {
         $('.remove-excerpt-from-within-form-modal').click(function() {
             const excerptId = $(this).attr('id').slice(22);
             $(this).addClass('no-show');
-            $("#add-excerpt-" + excerptId).removeClass('no-show');
-            $("#remove-excerpt-" + excerptId).addClass('no-show');
-            $("#remove-excerpt-" + excerptId).closest("li").removeClass('selected-excerpt');
+            $(".add-excerpt-" + excerptId).removeClass('no-show');
+            $(".remove-excerpt-" + excerptId).addClass('no-show');
+            $(".remove-excerpt-" + excerptId).closest("li").removeClass('selected-excerpt');
             $(this).closest('.li-container').addClass('no-show');
             chosenExcerpts = chosenExcerpts.filter(x => x.id !== excerptId);
 
